859c6b3e85c7c057ec12c222a09750e9ceb2e601
Francine Craven <cravenf@users.noreply.github.com>
2014-10-26 23:37:56 -0700

    Create main.c
    uses mbed.h's DigitalOut, absent red and green LEDs

---
+++ hw3/pgm_example_14_3/main.c
@@ -0,0 +1,64 @@
+/*
+ * Program Example 14.3:
+ *  Program control registers to flash different color LEDs. LEDs connect to mbed pins 25 and 26. Switch input to pin 9.
+ */
+#define MBED
+#ifdef MBED
+#include "mbed.h"
+DigitalOut myled1(LED1), myled2(LED2), myled3(LED3), myled4(LED4);
+#endif
+void delay(float);
+
+// Define digital I/O registers
+#define FIO0DIR0 (*( volatile unsigned char *)(0x2009C000))
+#define FIO0PIN0 (*( volatile unsigned char *)(0x2009C014))
+#define FIO2DIR0 (*(volatile unsigned char *)(0x2009C040))
+#define FIO2PIN0 (*(volatile unsigned char *)(0x2009C054))
+
+int main() {
+    char a, b, i;
+    // set all bits of port 0 byte 0 to input
+    FIO0DIR0=0x00;
+    // set port 2 byte 0 to output
+    FIO2DIR0=0xFF;
+    while(1) {
+        // Reverse the order of LED flashing based on switch position
+        if (FIO0PIN0&0x01==1){  // bit test port 0 pin 0 (mbed pin 9)
+            a=0x01;
+            b=0x02;
+#ifdef MBED
+            myled1 = 1;
+            myled3 = 0;
+#endif
+        } else {
+            a=0x02;
+            b=0x01;
+#ifdef MBED
+            myled1 = 0;
+            myled3 = 1;
+#endif
+        }
+        FIO2PIN0 |= a;
+        delay(0.25);
+        FIO2PIN0 &= ~a;
+        delay(0.25);
+        for (i=0;i<3;i++){
+            FIO2PIN0 |= b;
+            delay(0.25);
+            FIO2PIN0 &= ~b;
+            delay(0.25);
+        }
+    } // end while
+}
+
+void delay(float seconds) {
+#ifdef MBED
+    wait(seconds);
+#else
+    int j;           //loop variable j
+    for (j=0; j<10000000; j++) {
+        j++;
+        j--;           //waste time
+    }
+#endif
+}
