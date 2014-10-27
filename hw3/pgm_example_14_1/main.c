afcfbaa00564b0525f27ee8146a7c4a6ada7eab9
Francine Craven <cravenf@users.noreply.github.com>
2014-10-27 16:38:23 -0700

    Update main.c
    disabled mbed library after inserting a green LED in breadboard and rewiring

--- hw3/pgm_example_14_1/main.c
+++ hw3/pgm_example_14_1/main.c
@@ -1,10 +1,10 @@
 /*
  * Program Example 14.1: Sets up a digital output pin using control registers, and flashes an led.
  */
-#define MBED
+//#define MBED
 #ifdef MBED
 #include "mbed.h"
-DigitalOut myled1(LED1), myled2(LED2), myled3(LED3), myled4(LED4);
+DigitalOut greenled(p6);  //define and name a digital output on pin 6
 #endif
 // Define addresses of digital i/o control registers as pointers to volatile data
 #define FIO2DIR0    (*(volatile unsigned char *)(0x2009C040))
@@ -14,21 +14,21 @@ void delay(float seconds);
 
 int main()
 {
-   FIO2DIR0=0xFF;  // set port 2, lowest byte to output
+    FIO2DIR0=0xFF;  // set port 2, lowest byte to output
     while(1) {
         FIO2PIN0 |= 0x01;  // OR bit 0 with 1 to set pin high
 #ifdef MBED
-        myled2 = 1;
+        greenled = 1;
 #endif
         delay(0.25);
         FIO2PIN0 &= ~0x01; // AND bit 0 with 0 to set pin low
 #ifdef MBED
-        myled2 = 0;
+        greenled = 0;
 #endif
         delay(0.25);
     }
 }
-
+//delay function
 void delay(float seconds)
 {
 #ifdef MBED
