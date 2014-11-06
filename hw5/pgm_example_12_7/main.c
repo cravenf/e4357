433163c3e96629e8141b62ed3a7facb4572acc80
Francine Craven <cravenf@users.noreply.github.com>
2014-11-06 10:32:50 -0800

    Update main.c

--- hw5/pgm_example_12_7/main.c
+++ hw5/pgm_example_12_7/main.c
@@ -4,11 +4,26 @@
 #include "mbed.h"
 #include "Ethernet.h"
 Ethernet eth;                   // The Ethernet object
-char data[]={0xB9,0x46};        // Define the data values
-int main() {                      
-    while (1) {                    
-        eth.write(data,0x02);   // Write the package
+char data[] = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG";        // Define the data values
+
+// Ethernet object
+Serial pc(USBTX, USBRX);    // tx, rx for host terminal coms
+char buf[0xFF];             // create a large buffer to store data
+
+int main() {                 
+    while (1) {               
+        eth.write(data,sizeof(data));   // Write the package
         eth.send();             // Send the package
         wait(0.2);              // wait 200 ms
+
+        int size = eth.receive();                   // get size of incoming data packet
+        if (size > 0) {                             // if packet received
+            eth.read(buf, size);                    // read packet to data buffer
+            pc.printf("size = %d data = ",size);    // print to screen
+            for (int i=0;i<size;i++) {              // loop for each data byte
+                pc.printf("%c",buf[i]);            // print data to screen
+            }
+            pc.printf("\n\r");
+        }
     }
 }
