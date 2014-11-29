7285c7346fb1fac17f0e29e357c09ac03d77bf4d
Francine Craven <cravenf@users.noreply.github.com>
2014-11-29 15:51:03 -0800

    Update main.cpp

--- project/websocketclient/main.cpp
+++ project/websocketclient/main.cpp
@@ -1,7 +1,9 @@
 #include "mbed.h"
 #include "EthernetInterface.h"
 #include "Websocket.h"
+#include "time.h"
 
+//Serial pc(USBTX,USBRX);
 Ticker flash;
 DigitalOut led(LED1);
 void flashLED(void){led = !led;}
@@ -16,45 +18,62 @@ int main()
     
     EthernetInterface eth;
     eth.init(); // let dhcp assign ip address
-    int ret = eth.connect();
+    int n, ret = eth.connect();
 
+    printf("\n\r");
     if (0 == ret) {
-        printf("IP address is %s\n", eth.getIPAddress());
+        printf("IP address is %s\n\r", eth.getIPAddress());
     } else {
         error("eth.connect() returned %d\n\r", ret);
     }
 
-    // open http://sockets.mbed.org/demo/viewer to view
-//    Websocket ws("ws://sockets.mbed.org:443/ws/demo/rw");
     // open http://sockets.mbed.org/sensor/viewer to view
     Websocket ws("ws://sockets.mbed.org:443/ws/sensor/rw");
     ws.connect();
     char str[100];
+
+    UDPSocket sock;
+    ret = sock.init();
+//    printf("sock.init() returned %d\n\r", ret);  
+    Endpoint nist;
+    nist.set_address("utcnist.colorado.edu", 37);  
+    char outbuf[4];
+    time_t timeRes;
+    char localtime[40] = {0}, *p = localtime;
     
-    for(int i=0; i<0x7fffffff; ++i) {
-        
+    for(;;) {
+
+        ret = sock.sendTo(nist, "\0", 1);
+//        printf("sock.sendTo() returned %d\n\r", ret);
+
+        n = sock.receiveFrom(nist, outbuf, sizeof(outbuf));
+//        printf("sock.receiveFrom() returned %d\n\r", n);
+    
+        timeRes = ntohl(*((unsigned int*)outbuf));
+//        printf("Received %d bytes from server %s on port %d: %u seconds since 1/01/1900 00:00 GMT\n\r", n, nist.get_address(), nist.get_port(), timeRes);
+        p = ctime(&timeRes);
 
         myled2 = switch_ip2;
         myled3 = switch_ip2;
 
         // reflect sensor input
         if (switch_ip2)
-            sprintf(str, "%d MOTION", i);
+            sprintf(str, "MOTION %s", p);
         else
-            sprintf(str, "%d no motion", i);
+            sprintf(str, "no motion %s", p);
         ws.send(str);
     
         // clear the buffer and wait briefly
         memset(str, 0, sizeof(str));
-        wait(0.5f);
+        wait(5);
     
         // websocket server should echo whatever we sent it
         if (ws.read(str)) {
-            printf("rcv'd: %s\n\r", str);
+            printf("%s\n\r", str);
         }
     }
+
+    sock.close();
     ws.close();
     eth.disconnect();
-    
-    while(true);
 }
